ead894b7233c06645dec46e658b778e7ec84580f
gabrielmk666 <44278773+gabrielmk666@users.noreply.github.com>
2020-04-21 16:54:03 -0300

    Começando do 0

--- firstApp/ios/Pods/Target Support Files/Pods-firstApp-tvOSTests/Pods-firstApp-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firstApp_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_firstApp_tvOSTests
-@end
--- firstApp/ios/Pods/Target Support Files/Pods-firstApp/Pods-firstApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firstApp : NSObject
-@end
-@implementation PodsDummy_Pods_firstApp
-@end
