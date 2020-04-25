39a7dc18ed1ffe4d4cca8b2931a9fbc2f36cfdc9
gabrielmk666 <44278773+gabrielmk666@users.noreply.github.com>
2020-04-25 02:11:43 -0300

    Incremento Telas

---
+++ randomProjetct/ios/Pods/Target Support Files/Pods-randomProjetct-randomProjetctTests/Pods-randomProjetct-randomProjetctTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_randomProjetct_randomProjetctTests : NSObject
+@end
+@implementation PodsDummy_Pods_randomProjetct_randomProjetctTests
+@end
---
+++ randomProjetct/ios/Pods/Target Support Files/Pods-randomProjetct-tvOS/Pods-randomProjetct-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_randomProjetct_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_randomProjetct_tvOS
+@end
