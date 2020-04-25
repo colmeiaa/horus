1899c7c3f6582667639c2f94744076d1909fb9fd
gabrielmk666 <44278773+gabrielmk666@users.noreply.github.com>
2020-04-25 02:18:45 -0300

    FINAL

---
+++ randomProjetct/src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+export default function login() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>
+            Tela dos cards
+        </Text>
+    </View>
+  );
+}
