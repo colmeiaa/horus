83aaaa008eaab93bd3586d462644130559b6a885
gabrielmk666 <44278773+gabrielmk666@users.noreply.github.com>
2020-04-25 23:39:00 -0300

    delete all

--- randomProjetct/ios/Pods/Target Support Files/Pods-randomProjetct-randomProjetctTests/Pods-randomProjetct-randomProjetctTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_randomProjetct_randomProjetctTests : NSObject
-@end
-@implementation PodsDummy_Pods_randomProjetct_randomProjetctTests
-@end
